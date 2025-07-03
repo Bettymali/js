8ee85d215a0c3fee2a0a30bee50cd383966f0c13
Bettymali <Bmanasse@student.atlantatech.edu>
2025-07-02 22:53:08 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Countdown from './Countdown';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>1-Minute Countdown</h1>
+      <Countdown />
+    </div>
+  );
+}
+
+export default App;
